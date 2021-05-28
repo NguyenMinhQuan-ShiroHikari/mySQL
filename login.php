182099761d7508642fd5b2d286302bfd2cdc731b
Nguyễn Minh Quân <83341826+NguyenMinhQuan-ShiroHikari@users.noreply.github.com>
2021-05-28 09:49:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
     <div id="frm">
